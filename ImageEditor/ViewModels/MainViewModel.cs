311d3aef845cfd2ae2adf0c7b77d3fb463f1436d
Yura95st <yura95st@gmail.com>
2015-04-27 14:26:04 +0300

    Modify MainView: add new layout. ModifyMainViewModel: add new ViewModels properties.

--- ImageEditor/ViewModels/MainViewModel.cs
+++ ImageEditor/ViewModels/MainViewModel.cs
@@ -2,5 +2,41 @@
 {
     public class MainViewModel
     {
+        public MainViewModel()
+        {
+            this.InitViewModels();
+        }
+
+        public EditorViewModel EditorViewModel
+        {
+            get;
+            private set;
+        }
+
+        public FooterViewModel FooterViewModel
+        {
+            get;
+            private set;
+        }
+
+        public LeftPanelViewModel LeftPanelViewModel
+        {
+            get;
+            private set;
+        }
+
+        public TopPanelViewModel TopPanelViewModel
+        {
+            get;
+            private set;
+        }
+
+        private void InitViewModels()
+        {
+            this.EditorViewModel = new EditorViewModel();
+            this.FooterViewModel = new FooterViewModel();
+            this.LeftPanelViewModel = new LeftPanelViewModel();
+            this.TopPanelViewModel = new TopPanelViewModel();
+        }
     }
 }
