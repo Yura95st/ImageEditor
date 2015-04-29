d4f19c89d57479ac60d52ad307fb2a27ab0a883a
Yura95st <yura95st@gmail.com>
2015-04-30 00:06:49 +0300

    Modify MainCommands class: replace GalaSoft.MvvmLight.Command namespace with GalaSoft.MvvmLight.CommandWpf to fix issue with re-evaluating CanExecute() method of RelayCommand.

--- ImageEditor/Commands/Concrete/MainCommands.cs
+++ ImageEditor/Commands/Concrete/MainCommands.cs
@@ -2,7 +2,7 @@
 {
     using System.Windows.Input;
 
-    using GalaSoft.MvvmLight.Command;
+    using GalaSoft.MvvmLight.CommandWpf;
 
     using ImageEditor.Commands.Abstract;
     using ImageEditor.Utils;
