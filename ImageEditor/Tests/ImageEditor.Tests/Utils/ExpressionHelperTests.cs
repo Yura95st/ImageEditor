3500327f96d870582bf1d12ec1e8f5377e72d9e2
Yura95st <yura95st@gmail.com>
2015-04-30 18:02:25 +0300

    Add ExpressionHelper utility class.

---
+++ ImageEditor/Tests/ImageEditor.Tests/Utils/ExpressionHelperTests.cs
@@ -0,0 +1,57 @@
+﻿namespace ImageEditor.Tests.Utils
+{
+    using System;
+
+    using ImageEditor.Utils;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExpressionHelperTests
+    {
+        [Test]
+        public void GetPropertyName_PropertyLambdaIsInvalid_Throws()
+        {
+            // Arrange
+            SomeClass someClass = new SomeClass();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => ExpressionHelper.GetPropertyName(() => someClass.SomeMethod()));
+        }
+
+        [Test]
+        public void GetPropertyName_PropertyLambdaIsNull_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => ExpressionHelper.GetPropertyName<object>(null));
+        }
+
+        [Test]
+        public void GetPropertyName_PropertyLambdaIsValid_ReturnsValidPropertyName()
+        {
+            // Arrange
+            SomeClass someClass = new SomeClass();
+
+            // Act & Assert
+            Assert.AreEqual("SomeProperty", ExpressionHelper.GetPropertyName(() => someClass.SomeProperty));
+        }
+
+        #region Nested type: SomeClass
+
+        private class SomeClass
+        {
+            public object SomeProperty
+            {
+                get;
+                set;
+            }
+
+            public object SomeMethod()
+            {
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ImageEditor/Utils/ExpressionHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace ImageEditor.Utils
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public static class ExpressionHelper
+    {
+        // <summary>
+        // Get the name of a static or instance property from a property access lambda.
+        // </summary>
+        // <typeparam name="T">Type of the property.</typeparam>
+        // <param name="propertyLambda">Lambda expression of the form: '() => Class.Property' or '() => object.Property'.</param>
+        // <returns>The name of the property.</returns>
+
+        #region Public Methods
+
+        public static string GetPropertyName<T>(Expression<Func<T>> propertyLambda)
+        {
+            Guard.NotNull(propertyLambda, "propertyLambda");
+
+            MemberExpression me = propertyLambda.Body as MemberExpression;
+
+            if (me == null)
+            {
+                throw new ArgumentException(
+                "You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+            }
+
+            return me.Member.Name;
+        }
+
+        #endregion
+    }
+}
