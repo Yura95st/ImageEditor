76929d676de0e418ebe849ecf825da58cb43f5fa
Yura95st <yura95st@gmail.com>
2015-05-06 15:00:41 +0300

    Add new Show/HideCroppingRectangleMessage classes.

---
+++ ImageEditor/Messages/HideCroppingRectangleMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace ImageEditor.Messages
+{
+    using GalaSoft.MvvmLight.Messaging;
+
+    public class HideCroppingRectangleMessage : NotificationMessageAction<string>
+    {
+        public HideCroppingRectangleMessage(object sender)
+            : base(sender, "HideCroppingRectangleMessage", callback =>
+            {
+            })
+        {
+        }
+    }
+}
---
+++ ImageEditor/Messages/ShowCroppingRectangleMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace ImageEditor.Messages
+{
+    using GalaSoft.MvvmLight.Messaging;
+
+    public class ShowCroppingRectangleMessage : NotificationMessageAction<string>
+    {
+        public ShowCroppingRectangleMessage(object sender)
+            : base(sender, "ShowCroppingRectangleMessage", callback =>
+            {
+            })
+        {
+        }
+    }
+}
